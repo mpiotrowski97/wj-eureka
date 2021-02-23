8f263961f41a4c20b4b224dae441bf8753b0e8a2
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2021-02-23 07:22:05 +0100

    Basic project structure

---
+++ src/test/java/pl/michalpiotrowski/wjeureka/WjEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalpiotrowski.wjeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
